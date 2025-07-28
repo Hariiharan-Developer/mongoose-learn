6d1284636d88a771a8957ff3475b27ab08f643d9
Hariharan.s <hreehs1428@gmail.com>
2025-07-28 12:05:33 +0530

    multiple data created

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const User = require('./models/userModel');
 
 require('dotenv').config({ path: './config/.env' });
 
@@ -10,3 +11,33 @@ mongoose.connect(process.env.CLOUD_CON)
 .catch(()=>{
     console.log('Database connection error')
 })
+
+// const user =new User({
+//     name: 'Hariharan',
+//     age:24
+// })
+// user.save().then(()=>{
+//     console.log('User Saved')
+// })
+
+async function run(){
+    try{
+          const newuser = await User.create({
+      name: "Virat Kholi",
+      age: 39,
+      hobbies:['Sports','Music'],
+      addres:{
+        street:'2nd Street'
+      },
+      email:'test@harii.com'
+    }); 
+    console.log(newuser)
+    }
+    catch(e){
+        console.log(e.message)
+    }
+ 
+
+
+}
+run()
--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,40 @@
 const mongoose =require('mongoose');
 
 //schema:
+const addresSchema =new mongoose.Schema({
+      
+        city:String,
+        street:String
 
-const userSchema =new mongoose.schema({
+})
+
+const userSchema =new mongoose.Schema({
     name:String,
-    age:Number,
+    age:{
+         type:Number,
+        min:10,
+        max:40,
+        validate:{
+            validator: v => v % 2 == 0,
+            message: props=>`${props.value} is not number`
+        }
+
+    },
+    email:{
+        type: String,
+        required: true,
+        lowercase:true
+
+    },
+    createdAt:{
+        type:Date,
+        // default: new Date
+        default:()=>Date.now()
+    },
+    updatedAt:Date,
+    bestFriend:mongoose.Schema.Types.ObjectId,
+    hobbies:[String],
+    addres:addresSchema
 
 })
 
